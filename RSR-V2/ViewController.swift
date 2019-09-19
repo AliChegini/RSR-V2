476d16b31b1ff532fd80c7041b7aec90a7579ee9
Ali Chegini <ali.c.engineer@gmail.com>
2019-09-19 13:44:33 +0200

    initial commit

---
+++ RSR-V2/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RSR-V2
+//
+//  Created by Ehsan on 19/09/2019.
+//  Copyright © 2019 Ali C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world")
+    }
+
+}
